d5298e630e14444e4014fda457dcf67b16cc6530
Suraez <oberais50@gmail.com>
2025-09-12 15:41:53 -0400

    add bert deploymemt

--- openwhisk-bench/deploy_functions.sh
+++ openwhisk-bench/deploy_functions.sh
@@ -7,7 +7,7 @@
 # cd build
 # zip -r index.zip *
 
-# wsk -i action update mm --kind python:3.11 --main main --memory 512 --timeout 60000 index.zip
+# wsk -i action update mm --kind python:3.11 --main main --memory 2048 --timeout 60000 index.zip
 
 
 # # Chamelon
@@ -49,8 +49,7 @@ cp -R src/* build
 cd build
 zip -r index.zip *
 
-wsk -i action update video-process --kind python:3.11 --main main --memory 512 --timeout 60000 index.zip
-
+wsk -i action update video-process --kind python:3.11 --main main --memory 1024 --timeout 60000 index.zip --web true
 
 # cd ../../
 
@@ -64,3 +63,29 @@ wsk -i action update video-process --kind python:3.11 --main main --memory 512 -
 # zip -r index.zip *
 
 # wsk -i action update fc --kind python:3.11 --main main --memory 512 --timeout 60000 index.zip
+
+cd ../../
+cd bert
+
+rm -rf build
+mkdir build
+
+cp -R src/* build
+cd build
+zip -r index.zip *
+
+wsk -i action update bert --kind python:3.12 --main main --memory 1560 --timeout 60000 index.zip --web true
+
+wsk -i action invoke bert -r
+# cd ../..
+
+# cd image-recognition
+
+# rm -rf build
+# mkdir build
+
+# cp -R src/* build
+# cd build
+# zip -r index.zip *
+
+# wsk -i action update ir --kind python:image --main main --memory 2048 --timeout 60000 index.zip --web true
